4e0b5bcd70087897a7a2a4abd55bd8ae6b3e494d
mark-ypq <529912343@qqq.com>
2017-03-14 11:03:17 +0800

    Revert "Revert "add""
    This reverts commit abf69692379a73e6122b15decbb7201730fe564d.

--- app/src/main/java/com/markypq/gpshook/MainActivity.java
+++ app/src/main/java/com/markypq/gpshook/MainActivity.java
@@ -3,17 +3,24 @@ package com.markypq.gpshook;
 import android.Manifest;
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
@@ -22,8 +29,12 @@ import android.widget.Toast;
 
 import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.Random;
 
-public class MainActivity extends Activity {
+import de.robv.android.xposed.XSharedPreferences;
+import de.robv.android.xposed.XposedBridge;
+
+public class MainActivity  extends AppCompatActivity {
     TextView tv;
     EditText lan, lon,acc;
     CheckBox enableHook;
@@ -39,14 +50,31 @@ public class MainActivity extends Activity {
         lon = (EditText) findViewById(R.id.lon);
         acc= (EditText) findViewById(R.id.acc);
         enableHook = (CheckBox) findViewById(R.id.enableHook);
+        initView();
+        locationManager = (LocationManager) MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
+        getLocation(null);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode==0x01&&resultCode==Activity.RESULT_OK){
+            SharedPreferences sp = getSharedPreferences("markypq", MODE_WORLD_READABLE);
+            SharedPreferences.Editor e = sp.edit();
+            e.putString("lan", data.getDoubleExtra("lan",0)+"");
+            e.putString("lon", data.getDoubleExtra("lon",0)+"");
+            e.commit();
+            initView();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void initView() {
         SharedPreferences sp = getSharedPreferences("markypq", MODE_WORLD_READABLE);
         lan.setText(sp.getString("lan", ""));
         lon.setText(sp.getString("lon", ""));
         acc.setText(sp.getString("acc",""));
         enableHook.setChecked(sp.getBoolean("enableHook",true));
-        locationManager = (LocationManager) MainActivity.this.getSystemService(Context.LOCATION_SERVICE);
-        getLocation(null);
-
     }
 
     @TargetApi(23)
@@ -109,6 +137,16 @@ public class MainActivity extends Activity {
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 200,1, mlistener);
     }
 
+    public void map(View view) {
+        Intent intent =new Intent(MainActivity.this,MapActivity.class);
+        SharedPreferences sp = getSharedPreferences("markypq", MODE_WORLD_READABLE);
+            double latitude = Double.valueOf(sp.getString("lan","117.536246"));
+            double longtitude = Double.valueOf(sp.getString("lon","36.681752"));
+        intent.putExtra("lan",latitude);
+        intent.putExtra("lon",longtitude);
+        startActivityForResult(intent,0x01 );
+    }
+
     private class TestLocationListener implements LocationListener {
         @Override
         public void onLocationChanged(Location location){
@@ -155,5 +193,32 @@ public class MainActivity extends Activity {
         // 设置对电源的需求
         criteria.setPowerRequirement(Criteria.POWER_LOW);
         return criteria;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.setting:
+
+            case R.id.about:
+                try {
+                    String url="mqqwpa://im/chat?chat_type=wpa&uin=2039609991";
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                }catch (Exception e){
+                    ClipboardManager myClipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);;
+                    myClipboard.setPrimaryClip(ClipData.newPlainText("text", "2039609991"));
+                    Toast.makeText(MainActivity.this,"作者QQ已经复制",Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.donate:
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("HTTPS://QR.ALIPAY.COM/FKX04652BHHIVRVZIABFB1".toLowerCase())));
+                break;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
